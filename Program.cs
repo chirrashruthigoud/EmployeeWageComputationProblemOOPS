730ec6f2b2b63ae7603cd62d3fd32fc4d9a77b81
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-02-17 18:26:30 +0530

    Employee Multiple Wage

--- Program.cs
+++ Program.cs
@@ -7,8 +7,13 @@ namespace EmployeeWageComputationUsingOOPS
     {
         public static void Main(string[] args)
         {
-            RefactorCode em = new RefactorCode();
-            em.EmployeeWage();
+            MultipleWage e1 = new MultipleWage();
+            int a = e1.EmployeeWage("BridgeLabz", 20, 20, 100);
+            Console.WriteLine("First Company :" + a);
+
+            MultipleWage e2 = new MultipleWage();
+            int b = e1.EmployeeWage("AkadBakad", 20, 20, 100);
+            Console.WriteLine("Second Company : " + b);
         }
     }
 }
