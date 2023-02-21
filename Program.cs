81260f36adf1f85d92a3e1a9e5df5c16632a1055
chirrashruthigoud <chirrashruthigoud4@gmail.com>
2023-02-21 17:13:45 +0530

    Ability to save the Total Wage for Each Company

--- Program.cs
+++ Program.cs
@@ -7,13 +7,13 @@ namespace EmployeeWageComputationUsingOOPS
     {
         public static void Main(string[] args)
         {
-            MultipleWage e1 = new MultipleWage();
-            int a = e1.EmployeeWage("BridgeLabz", 20, 20, 100);
-            Console.WriteLine("First Company :" + a);
-
-            MultipleWage e2 = new MultipleWage();
-            int b = e1.EmployeeWage("AkadBakad", 20, 20, 100);
-            Console.WriteLine("Second Company : " + b);
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+            TotalEmpWage dMart = new TotalEmpWage("DMart", 20, 2, 10);
+            TotalEmpWage reliance = new TotalEmpWage("Reliance", 10, 4, 20);
+            dMart.computeEmpWage();
+            Console.WriteLine(dMart.toString());
+            reliance.computeEmpWage();
+            Console.WriteLine(reliance.toString());
         }
     }
 }
